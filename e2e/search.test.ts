604fcc215bae8babe7d912e070b0c8b476572b1e
Andres Lemus <aclmadrid04@gmail.com>
2023-03-28 18:20:05 -0500

    test: group e2e test in oone

--- e2e/search.test.ts
+++ e2e/search.test.ts
@@ -1,15 +1,18 @@
 import { expect, test } from "@playwright/test";
 
 test.describe("Search", () => {
-  test.beforeEach(async ({ page }) => {
+  const query = "capital";
+
+  test(`Search for '${query}' includes filters, results count and list`, async ({
+    page,
+  }) => {
     /**
      * Usually we hace a staging env where we control the data of our database
      * so it is easier to avoid false positives.
      */
     await page.goto("https://search-engine.andreslemusm.com/");
-  });
 
-  test("Has call to action message when no search", async ({ page }) => {
+    // Has call to action message by default
     await expect(
       page.getByRole("img", { name: /woman organizing blocks on a ui/i })
     ).toBeVisible();
@@ -21,32 +24,24 @@ test.describe("Search", () => {
     await expect(
       page.getByRole("heading", { name: /what are you searching for\?/i })
     ).toBeVisible();
-  });
-
-  test("Search for 'capital' includes filters, results count and list", async ({
-    page,
-  }) => {
-    const mockedSearch = "capital";
 
     const searchInput = page.getByRole("textbox", { name: /search/i });
-    await searchInput.fill(mockedSearch);
+    await searchInput.fill(query);
     await searchInput.press("Enter");
 
-    // I have all(default), organization, and topic filters.
+    // Has all, organization, and topic filters.
     await expect(page.getByRole("link", { name: /all/i })).toBeVisible();
     await expect(
       page.getByRole("link", { name: /organization/i })
     ).toBeVisible();
     await expect(page.getByRole("link", { name: /topic/i })).toBeVisible();
 
-    // I have the results count
+    // Has the results count
     await expect(
-      page.getByText(
-        new RegExp(`we found \\w+ results for "${mockedSearch}"`, "i")
-      )
+      page.getByText(new RegExp(`we found \\w+ results for "${query}"`, "i"))
     ).toBeVisible();
 
-    // I have a list of result
+    // Has a list of result
     expect(await page.getByRole("listitem").count()).toBeGreaterThan(0);
   });
 });
